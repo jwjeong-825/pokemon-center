f9f17de9c8da02e857df615d918f3a33a61f28ce
jwjeong-825 <tobotyjjj111@naver.com>
2026-05-02 21:30:30 +0900

    feat: add pokemon create and read api

--- app/main.py
+++ app/main.py
@@ -2,6 +2,33 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+pokemon_db = []
+
 @app.get("/")
 def home():
     return {"message": "Welcome to Juwon's Pokemon Center"}
+
+@app.post("/pokemon")
+def add_pokemon(name: str, level: int):
+    pokemon = {
+	"id": len(pokemon_db) + 1,
+        "name": name,
+        "level": level
+    }
+
+    pokemon_db.append(pokemon)
+
+    return {
+        "message": "Pokemon added successfully",
+        "pokemon": pokemon
+    }
+@app.get("/pokemon")
+def get_pokemon():
+    return pokemon_db
+@app.get("/pokemon/{pokemon_id}")
+@app.get("/pokemon/{pokemon_id}")
+def get_one_pokemon(pokemon_id: int):
+    if pokemon_id <= 0 or pokemon_id > len(pokemon_db):
+        return {"message": "Pokemon not found"}
+
+    return pokemon_db[pokemon_id - 1]
