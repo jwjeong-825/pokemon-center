f30b51cfa03b537903cb768e227779bd81908c6f
jwjeong-825 <tobotyjjj111@naver.com>
2026-05-04 09:46:30 +0900

    feat: add discharge api by pokemon name

--- app/main.py
+++ app/main.py
@@ -32,3 +32,13 @@ def get_one_pokemon(pokemon_id: int):
         return {"message": "Pokemon not found"}
 
     return pokemon_db[pokemon_id - 1]
+@app.delete("/pokemon/name/{name}")
+def discharge_pokemon(name: str):
+    for pokemon in pokemon_db:
+        if pokemon["name"] == name:
+            pokemon_db.remove(pokemon)
+            return {
+                "message": f"{name} 퇴원 완료! 트레이너에게 돌아갔습니다."
+            }
+
+    return {"error": "해당 포켓몬을 찾을 수 없습니다."}
