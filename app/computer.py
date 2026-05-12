6ddd2b1160ca13133d66c5b6019d5ae0650ca1cf
jwjeong-825 <tobotyjjj111@naver.com>
2026-05-12 15:36:35 +0900

    Improve Swagger API documentation

--- app/computer.py
+++ app/computer.py
@@ -6,10 +6,16 @@ from app.database import (
     delete_pokemon_by_name_db
 )
 
-router = APIRouter()
+router = APIRouter(
+    tags=["Pokemon Computer"]
+)
 
 
-@router.post("/pokemon")
+@router.post(
+    "/computer/deposit",
+    summary="포켓몬 맡기기",
+    description="트레이너의 포켓몬을 PC에 저장합니다."
+)
 def add_pokemon(name: str, level: int):
 
     add_pokemon_db(name, level)
@@ -19,13 +25,21 @@ def add_pokemon(name: str, level: int):
     }
 
 
-@router.get("/pokemon")
+@router.get(
+    "/computer",
+    summary="맡긴 포켓몬 전체 조회",
+    description="PC에 저장된 모든 포켓몬 목록을 조회합니다."
+)
 def get_pokemon():
 
     return get_pokemon_db()
 
 
-@router.get("/pokemon/{pokemon_id}")
+@router.get(
+    "/computer/{pokemon_id}",
+    summary="포켓몬 상세 조회",
+    description="특정 포켓몬의 정보를 조회합니다."
+)
 def get_one_pokemon(pokemon_id: int):
 
     pokemon = get_one_pokemon_db(pokemon_id)
@@ -36,7 +50,11 @@ def get_one_pokemon(pokemon_id: int):
     return pokemon
 
 
-@router.delete("/pokemon/name/{name}")
+@router.delete(
+    "/computer/discharge/{name}",
+    summary="포켓몬 찾기",
+    description="PC에서 포켓몬을 꺼내 트레이너에게 돌려줍니다."
+)
 def discharge_pokemon(name: str):
 
     deleted_count = delete_pokemon_by_name_db(name)
